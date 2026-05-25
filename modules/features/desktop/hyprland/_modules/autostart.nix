3a09f16697c087f6595084afb19c8ad470d70c20
sugjo <iluxa-com2001@yandex.ru>
2026-05-26 02:30:51 +0500

    add: wallpaper app

--- modules/features/desktop/hyprland/_modules/autostart.nix
+++ modules/features/desktop/hyprland/_modules/autostart.nix
@@ -2,6 +2,7 @@
     wayland.windowManager.hyprland.settings = {
         exec-once = [
             "noctalia-shell"
+            "hyprpaper"
             "[workspace 1 silent] ${osConfig.desktop.terminal.run} tmux"
             "[workspace 4 silent;noscreenshare] Telegram"
             "[workspace 4 silent;noanim;noscreenshare] command -v vesktop &> /dev/null && vesktop"
