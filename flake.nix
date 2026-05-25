9ddbc954dfc39156a5e9010b8c340b834a1ec7fd
sugjo <iluxa-com2001@yandex.ru>
2026-05-26 00:06:53 +0500

    update: flake.lock

--- flake.nix
+++ flake.nix
@@ -23,15 +23,20 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    # stylix = {
-    #   url = "github:nix-community/stylix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+  nixvim = {
+    url = "github:nix-community/nixvim/fca03f175902fe899a87872228bf69c1b43a8543";
+      inputs.nixpkgs.follows = "nixpkgs";
+  };
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
   };
