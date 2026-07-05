3ddbc0e4f74453e2b75845681b3e8cf80491c2cb
sugjo <iluxa-com2001@yandex.ru>
2026-07-05 07:50:19 +0500

    add: kitty module

--- modules/features/desktop/kitty.nix
+++ modules/features/desktop/kitty.nix
@@ -1,7 +1,28 @@
 {
-    flake.modules.nixos.kitty = { ... }: {
+    flake.modules.nixos.kitty = { lib, pkgs, ... }: {
+        desktop.terminal = {
+          bin = lib.mkDefault "${pkgs.kitty}/bin/kitty";
+          run = lib.mkDefault "${pkgs.kitty}/bin/kitty -e";
+        };
+
         hm = [{
-            programs.kitty.enable = true;
+            programs.kitty = {
+                enable = true;
+                font = {
+                    size = lib.mkForce 16.0;
+                };
+
+                settings = {
+    cursor_trail = 30;                 # Увеличиваем длину для плавности (по умолчанию 0, макс 64)
+    cursor_trail_start_threshold = 0;   # Шлейф появляется сразу при малейшем сдвиге
+    cursor_trail_decay = "0.05 0.2";    # Мягкое появление и быстрое исчезновение хвоста
+
+    cursor_shape = "block";
+    cursor_blink = true;
+
+                    confirm_os_window_close = 0;
+                };
+            };
         }];
     };
 }
