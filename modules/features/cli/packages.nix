1ccbe5330364a793129318c536fdf149965b0842
sugjo <iluxa-com2001@yandex.ru>
2026-07-06 18:22:32 +0500

    add: cli packages

--- modules/features/cli/packages.nix
+++ modules/features/cli/packages.nix
@@ -2,6 +2,8 @@
     flake.modules.nixos.cli = { pkgs, ... }: {
         environment.systemPackages = with pkgs; [
             # Utils
+            nix-index
+            lazygit
             comma
             eza
         ];
